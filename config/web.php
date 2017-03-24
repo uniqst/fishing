86f7b43f638c239e19f018fe8981188d9edfc3af
Galifax <galifax94@gmail.com>
2017-03-24 15:44:18 +0200

    debuger

--- config/web.php
+++ config/web.php
@@ -57,7 +57,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['217.76.199.238', '::1'],
+        // 'allowedIPs' => ['217.76.199.238', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
