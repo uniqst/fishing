3a0679e8839f8e841e863c77d8b1edb23d6936dd
Galifax <galifax94@gmail.com>
2017-03-26 18:03:21 +0300

    order

---
+++ models/Order.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property string $id
+ * @property string $created_at
+ * @property string $update_at
+ * @property integer $qty
+ * @property double $sum
+ * @property string $status
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $address
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'update_at', 'qty', 'sum', 'name', 'email', 'phone', 'address'], 'required'],
+            [['created_at', 'update_at'], 'safe'],
+            [['qty'], 'integer'],
+            [['sum'], 'number'],
+            [['status'], 'string'],
+            [['name', 'email', 'phone', 'address'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'update_at' => 'Update At',
+            'qty' => 'Qty',
+            'sum' => 'Sum',
+            'status' => 'Status',
+            'name' => 'Name',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'address' => 'Address',
+        ];
+    }
+}
