ea41fc5b86817a5ed039fe2938528cea16c77752
Galifax <galifax94@gmail.com>
2017-03-27 16:10:54 +0300

    select parent id form

--- modules/admin/views/category/_form.php
+++ modules/admin/views/category/_form.php
@@ -2,17 +2,22 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Category;
+use yii\helpers\ArrayHelper;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Category */
 /* @var $form yii\widgets\ActiveForm */
+$cats = Category::find()->asArray()->all();
+$items = ArrayHelper::map($cats,'id','name');
 ?>
 
 <div class="category-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'parent_id')->textInput() ?>
+    <?= $form->field($model,'parent_id')->dropDownList($items);?>
 
     <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
 
