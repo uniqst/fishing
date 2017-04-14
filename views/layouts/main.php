23dc3776d24786cf599dcbdb274b2ddc23290304
Galifax <galifax94@gmail.com>
2017-04-14 11:42:26 +0300

    123123

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -76,20 +76,6 @@ $pages = Pages::find()->all();
   </div><!-- /.container-fluid -->
 </nav>
 
-<script>
-function autoSubmit()
-{
-    var formObject = document.forms['theForm'];
-    formObject.submit();
-}
-</script>
-</head>
-<body>
-<form name='theForm' id='theForm'>
-    <input type="radio" name="sort" <?php if ($sort == 'upload_time') { ?>checked='checked' <?php } ?>value="upload_time" onChange="autoSubmit();" />Recently Uploaded
-    <input type="radio" name="sort" <?php if ($sort == 'article') { ?>checked='checked' <?php } ?> value="article" onChange="autoSubmit();" /> Alphabetically
-    <input type="radio" name="sort" <?php if ($sort == 'year') { ?>checked='checked' <?php } ?> value="year" onChange="autoSubmit();" /> Most Recent
-</form>
     <?php
     $category =$this->params['key'] ;
     ?>
